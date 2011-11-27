1891148ed97b49657a9151e6e2b85c77de40507c
James Hibberd <james.d.hibberd@gmail.com>
2011-11-27 22:36:32 +0000

    App generates random graphs, solves them and displays the solutions in a browser.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import astarsearch
 import json
 import os.path
 import random
@@ -10,7 +11,10 @@ import tornado.websocket
 class MainHandler(tornado.web.RequestHandler):
 
     def get(self):
-        self.render('html/main.html')
+        constants = dict([\
+            ('NODE_TYPE_'+k, getattr(NodeType, k)) for\
+            k in dir(NodeType) if not k.startswith('__')])
+        self.render('html/main.html', constants=constants)
 
 
 class SubscribeHandler(tornado.websocket.WebSocketHandler):
@@ -23,31 +27,106 @@ class SubscribeHandler(tornado.websocket.WebSocketHandler):
         SubscribeHandler.subscribers.remove(self)
 
     @classmethod
-    def publish(cls, payload):
-        payload = json.dumps(payload)
+    def publish(cls, graph):
+        graph = json.dumps(graph)
         for s in cls.subscribers:
-            s.write_message(payload)
+            s.write_message(graph)
 
 
 class Graph(object):
     """2-dimensional array representing the graph to be traversed by the search
-    by the search algorithm.
+    algorithm.
     """
 
-    WIDTH = 15
-    HEIGHT = 10
+    @classmethod
+    def build(cls, width, height, node_providers):
+        graph = Graph(width, height)
+        [p.apply(graph) for p in node_providers]
+        return graph
 
-    def __init__(self):
-        self.data = [[0 for _ in range(self.WIDTH)] for _ in range(self.HEIGHT)]
-        self.old_point = (0, 0)
+    def __init__(self, width, height):
+        self.nodes =\
+            [[0 for _ in range(width)] for _ in range(height)]
+        self.width = width
+        self.height = height
 
-    def random(self):
-        x = random.randint(0, self.WIDTH-1)
-        y = random.randint(0, self.HEIGHT-1)
-        self.data[self.old_point[1]][self.old_point[0]] = 0
-        self.old_point = (x, y)
-        self.data[self.old_point[1]][self.old_point[0]] = 1
-        
+    def get(self, point):
+        x, y = point
+        return self.nodes[y][x]
+
+    def set(self, point, node_type):
+        x, y = point
+        self.nodes[y][x] = node_type
+
+    def set_many(self, points, node_type):
+        for x, y in points:
+            self.nodes[y][x] = node_type
+
+    def find_one(self, node_type):
+        for point, this_node_type in self._iter():
+            if this_node_type == node_type:
+                return point
+
+    def find_all(self, node_type):
+        for point, this_node_type in self._iter():
+            if this_node_type == node_type:
+                yield point
+
+    def _iter(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                p = (x, y)
+                yield p, self.get(p)
+
+
+class BearProvider(object):
+    
+    def apply(self, graph):
+        while True:
+            p = (
+                random.randint(0, graph.width-1),
+                random.randint(0, graph.height-1))
+            if graph.get(p) == NodeType.GRASS:
+                graph.set(p, NodeType.BEAR)
+                break
+
+
+class HoneyProvider(object):
+
+    def apply(self, graph):
+        while True:
+            p = (
+                random.randint(0, graph.width-1),
+                random.randint(0, graph.height-1))
+            if graph.get(p) == NodeType.GRASS:
+                graph.set(p, NodeType.HONEY)
+                break
+
+
+class TreeProvider(object):
+
+    def __init__(self, count):
+        self.count = count
+
+    def apply(self, graph):
+        remaining = self.count
+        while remaining:
+            p = (
+                random.randint(0, graph.width-1),
+                random.randint(0, graph.height-1))
+            if graph.get(p) == NodeType.GRASS:
+                graph.set(p, NodeType.TREE)
+                remaining -= 1
+
+
+class NodeType(object):
+    """Exhaustive list of graph node types."""
+    GRASS =     0
+    BEAR =      1
+    HONEY =     2
+    PATH =      3
+    TREE =      4
+
 
 class GraphMutation(object):
     """Controller that modifies node values within a graph in realtime and
@@ -70,13 +149,36 @@ class GraphMutation(object):
  
     def __init__(self):
         self.stop_signal = False
-        self.graph = Graph()
 
     def inst_start(self):
         while not self.stop_signal:
-            self.graph.random()
-            SubscribeHandler.publish(self.graph.data)
+            self.graph = Graph.build(
+                30, # Width
+                15, # Height
+                [
+                    BearProvider(),
+                    HoneyProvider(),
+                    TreeProvider(170), # Tree count
+                ])
+            # TODO(jhibberd) Only show after it's solved in case it can't be
+            # solved. Also need lots of comments. also don't show background
+            # for bear and honey while unsolved.
+            SubscribeHandler.publish(self.graph.nodes)
+            start = self.graph.find_one(NodeType.BEAR)
+            goal = self.graph.find_one(NodeType.HONEY)
+            closed_set = set(self.graph.find_all(NodeType.TREE))
+            algorithm =\
+                astarsearch.Algorithm(self.graph.width, self.graph.height)
+            path = algorithm.search(start, goal, closed_set) 
+            if not path:
+                # No path between start and goal.
+                continue
+            # TODO(jhibberd) The algorithm path shouldn't include the start.
+            path.remove(start)
+            self.graph.set_many(path, NodeType.PATH)
             time.sleep(2)
+            SubscribeHandler.publish(self.graph.nodes)
+            time.sleep(5)
 
     def inst_stop(self):
         self.stop_signal = True
--- static/main.js
+++ static/main.js
@@ -14,27 +14,112 @@ var draw = function(grid) {
     var graphHeight = grid.length;
     var graphWidth = grid[0].length;
 
-    var SQUARE_SIZE = 30;
+    var NODE_SIZE = 30;
     for (var y = 0; y < graphHeight; y++) {
         for (var x = 0; x < graphWidth; x++) {
-            var value = grid[y][x];
-            var color = "rgb(255, 255, 255)";
-            if (value == 1) {
-                color = "rgb(200, 0, 0)";
+            var screenX = x * NODE_SIZE;
+            var screenY = y * NODE_SIZE;
+            switch (grid[y][x]) {
+                case NODE_TYPE_BEAR:
+                    drawBear(ctx, screenX, screenY, NODE_SIZE);
+                    break;
+                case NODE_TYPE_HONEY:
+                    drawHoney(ctx, screenX, screenY, NODE_SIZE);
+                    break;
+                case NODE_TYPE_PATH:
+                    drawPath(ctx, screenX, screenY, NODE_SIZE);
+                    break;
+                case NODE_TYPE_TREE:
+                    drawTree(ctx, screenX, screenY, NODE_SIZE);
+                    break;
+                default:
+                    drawGrass(ctx, screenX, screenY, NODE_SIZE);
             }
-            ctx.fillStyle = color;  
-            ctx.fillRect(
-                x * SQUARE_SIZE,
-                y * SQUARE_SIZE,
-                SQUARE_SIZE,
-                SQUARE_SIZE);  
-            ctx.strokeRect(
-                x * SQUARE_SIZE,
-                y * SQUARE_SIZE,
-                SQUARE_SIZE,
-                SQUARE_SIZE);  
         }
     } 
+}
+
+// TODO(jhibberd) Abstract functions, they all take the same args.
+
+var drawTree = function(ctx, x, y, size) {
+    drawGrass(ctx, x, y, size);
+    ctx.fillStyle = "rgb(42, 92, 11)";  
+    ctx.beginPath();
+    ctx.moveTo(x + (size*.5), y + (size*.1));
+    ctx.lineTo(x + (size*.1), y + (size*.7)); 
+    ctx.lineTo(x + (size*.9), y + (size*.7));
+    ctx.lineTo(x + (size*.5), y + (size*.1));
+    ctx.fill();
+    ctx.fillRect(
+        x + (size*.4),
+        y + (size*.7),
+        size*.2,
+        size*.2);
+}
+
+var drawBear = function(ctx, x, y, size) {
+    drawPath(ctx, x, y, size);
+    ctx.fillStyle = "#63420b";
+    ctx.beginPath();
+    ctx.moveTo(x + (size*.1), y + (size*.8));
+    ctx.bezierCurveTo(
+        x + (size*.1),
+        y - (size*.1),
+        x + (size*.9),
+        y - (size*.1),
+        x + (size*.9),
+        y + (size*.8));
+    ctx.lineTo(x + (size*.1), y + (size*.8));
+    ctx.fill()
+    ctx.fillStyle = "rgb(255, 255, 255)";
+    ctx.beginPath();
+    ctx.arc(
+        x + (size*.43), 
+        y + (size*.3), 
+        size*.04, 
+        0, 
+        Math.PI*2, 
+        true);
+    ctx.arc(
+        x + (size*.57), 
+        y + (size*.3), 
+        size*.04, 
+        0, 
+        Math.PI*2, 
+        true);
+    ctx.fill()
+}
+
+var drawHoney = function(ctx, x, y, size) {
+    drawPath(ctx, x, y, size);
+    ctx.fillStyle = "rgb(234, 42, 21)"; 
+    ctx.beginPath(); 
+    ctx.arc(
+        x + (size*.5), 
+        y + (size*.5), 
+        size*.3, 
+        0, 
+        Math.PI*2, 
+        true);
+    ctx.fill();
+}
+
+var drawPath = function(ctx, x, y, size) {
+    ctx.fillStyle = "#a36c13";  
+    ctx.fillRect(
+        x,
+        y,
+        size,
+        size);  
+}
+
+var drawGrass = function(ctx, x, y, size) {
+    ctx.fillStyle = "rgb(128, 143, 18)";  
+    ctx.fillRect(
+        x,
+        y,
+        size,
+        size);  
 }
 
 var updater = {
